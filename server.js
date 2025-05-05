78fa75bd690961ee486aa09d29e79a13fa001ed1
Yuvraaj Narula <ynarula678@gmail.com>
2025-05-05 15:41:11 +0530

    base express server setup

---
+++ server.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+})
