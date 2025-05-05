9c924f2883d294396abce89c9a87e07ba11aa94c
Yuvraaj Narula <ynarula678@gmail.com>
2025-05-05 18:38:11 +0530

    cleanup

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const dbRoutes = require('./routes/dbOps');
+const {
+    databaseExists, createDatabase, dataInjection 
+} = require('./db.js');
 app.use(express.json());
-app.use('/api/db', dbRoutes);
-app.listen(process.env.PORT || 3000,()=>{
+app.listen(process.env.PORT || 3000,async()=>{
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    await databaseExists(process.env.DB_NAME).then(async (exists) => {
+        if (!exists.exists) {
+            console.log(`Database ${process.env.DB_NAME} does not exist. Creating...`);
+            await createDatabase(process.env.DB_NAME)
+            console.log(`Database ${process.env.DB_NAME} created successfully.`);
+        }
+        console.log(`Database ${process.env.DB_NAME} exists.`);
+        await dataInjection(process.env.DB_NAME).then(() => {
+            console.log(`Data injected into ${process.env.DB_NAME} successfully.`);
+        }).catch((error) => {
+            console.error('Error injecting data:', error);
+        });
+    })
 })
